d19b43ea2803ff38f9b898a0c6ae3d4290ed6101
Johnathan Aspinwall <jwaspin@gmail.com>
2024-04-18 10:47:57 -0600

    feat: Update mouse settings for better user experience

--- files/mac/defaults.sh
+++ files/mac/defaults.sh
@@ -2,6 +2,7 @@
 
 # Setting the default USB drive path
 USB_DRIVE_PATH="${1:-/Volumes/SKINNY}"
+MAC_SETTINGS_PATH="${USB_DRIVE_PATH}/dot/files/mac"
 
 defaults write NSGlobalDomain KeyRepeat -int 2
 defaults write NSGlobalDomain InitialKeyRepeat -int 15
@@ -10,9 +11,12 @@ defaults write NSGlobalDomain AppleActionOnDoubleClick -string "Minimize"
 defaults write NSGlobalDomain AppleInterfaceStyle -string "Dark"
 defaults write NSGlobalDomain NSPreferredWebServices -dict-add NSWebServicesProviderWebSearch -dict NSDefaultDisplayName DuckDuckGo NSProviderIdentifier com.duckduckgo
 
-USER_DICT_FILE="${USB_DRIVE_PATH}/path/to/dictionary_replacements.plist"
+USER_DICT_FILE="${MAC_SETTINGS_PATH}/dictionary_replacements.plist"
 defaults import NSGlobalDomain "$USER_DICT_FILE"
 
 # Set trackpad settings
-TRACKPAD_SETTINGS_FILE="${USB_DRIVE_PATH}/path/to/trackpad_settings.plist"
+TRACKPAD_SETTINGS_FILE="${MAC_SETTINGS_PATH}/trackpad_settings.plist"
 defaults import com.apple.AppleMultitouchTrackpad "$TRACKPAD_SETTINGS_FILE"
+
+MOUSE_SETTINGS_FILE="${MAC_SETTINGS_PATH}/mouse_settings.plist"
+defaults import com.apple.driver.AppleHIDMouse "$MOUSE_SETTINGS_FILE"
