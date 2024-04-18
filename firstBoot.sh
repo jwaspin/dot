c4de2bca0c9c057acdfb148583b5f0a2267348b2
Johnathan <jwaspin@users.noreply.github.com>
2024-04-18 11:29:55 -0600

    Feature/first-boot-script (#1)
    * feat: Add firstBoot.sh script for initial setup
    
    * chore: Add Homebrew dependencies and cask applications
    
    * chore: Update firstBoot.sh script for initial setup
    
    * feat: Add default settings for USB drive and trackpad
    
    This commit adds a new script, `defaults.sh`, which sets the default settings for the USB drive path and trackpad. It also imports user-specific dictionary replacements and trackpad settings from the USB drive.
    
    * feat: Add dictionary replacements and trackpad settings
    
    This commit adds dictionary replacements and trackpad settings to the repository. The `dictionary_replacements.plist` file contains user-specific dictionary replacements, while the `trackpad_settings.plist` file contains default trackpad settings. These files are necessary for the proper functioning of the application.
    
    * feat: Update mouse settings for better user experience
    
    * feat: Import dock settings for better user experience

---
+++ firstBoot.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+# Ensure the script exits on any error
+set -e
+
+# Setting the default USB drive path
+USB_DRIVE_PATH="${1:-/Volumes/SKINNY}"
+
+# Install Homebrew
+echo "Installing Homebrew..."
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# Check and install necessary packages from Brewfile located in the same directory
+if [[ ! -f "${USB_DRIVE_PATH}/dot/files/brew/Brewfile" ]]; then
+    echo "Brewfile not found in ${USB_DRIVE_PATH}/dot/files/brew/"
+else
+    echo "Installing packages from Brewfile..."
+    brew bundle --file="${USB_DRIVE_PATH}/dot/files/brew/Brewfile"
+fi
+
+# Clearing Dock Items
+defaults write com.apple.dock persistent-apps -array
+killall Dock
+
+# Set defaults
+if [[ ! -f "${USB_DRIVE_PATH}/dot/files/mac/defaults.sh" ]]; then
+    echo "defaults.sh not found in ${USB_DRIVE_PATH}/dot/files/mac/"
+else
+    echo "Setting defaults..."
+    /bin/bash "${USB_DRIVE_PATH}/dot/files/mac/defaults.sh" $USB_DRIVE_PATH
+fi
+
+# Install dotfiles
+if [[ ! -d "${USB_DRIVE_PATH}/dot/files" ]]; then
+  echo "Dotfiles directory not found in ${USB_DRIVE_PATH}/dot/files/"
+else
+  echo "Installing dotfiles..."
+  /bin/bash "${USB_DRIVE_PATH}/dot/install.sh" -ah
+fi
+
+# Configure SSH
+echo "Configuring SSH..."
+mkdir -p ~/.ssh
+
+# Copy all keys while preserving the directory structure
+cp -r ${USB_DRIVE_PATH}/ssh/ ~/.ssh/
+
+# Set correct permissions for all files in the keys directory
+find ~/.ssh/keys -type f -name '*.pub' -exec chmod 644 {} \;
+find ~/.ssh/keys -type f ! -name '*.pub' -exec chmod 600 {} \;
+
+# Ensure the entire dconf.d directory and contents have appropriate permissions
+chmod -R 700 ~/.ssh/dconf.d
+
+echo "SSH configuration completed."
+
+
+echo "Setup completed successfully."
